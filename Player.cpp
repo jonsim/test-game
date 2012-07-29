b32ed9e0566ac7eb5b6a212d455094e60ad5751e
Jonathan Simmonds <jonathansimmonds@gmail.com>
2012-07-29 16:05:50 +0100

    git is insanely shit, part 1.

---
+++ Player.cpp
@@ -0,0 +1,18 @@
+//---------- INCLUDES ----------//
+#include "Player.h"
+
+
+
+//---------- FUNCTIONS ----------//
+
+/// @brief  Constructor
+Player::Player (void)
+{
+}
+
+
+
+/// @brief  Deconstructor
+Player::~Player (void)
+{
+}
---
+++ Player.h
@@ -0,0 +1,17 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+
+//---------- INCLUDES ----------//
+
+
+
+//---------- DEFINITIONS ----------//
+class Player
+{
+public:
+    Player (void);
+    ~Player (void);
+};
+
+#endif // #ifndef PLAYER_H
